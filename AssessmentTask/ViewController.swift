15e2b224a7b1ca72f53a88069d1b0dd7f0925158
Korede612 <korede.oko-osi@nytengaletech.com>
2024-02-03 18:20:17 +0100

    Initial Commit

---
+++ AssessmentTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssessmentTask
+//
+//  Created by Oko-osi Korede on 03/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
